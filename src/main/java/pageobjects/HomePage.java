1b841b7480396743bc633f1a12c7689de6c7278a
ThanhHanh16 <thanhhanh0104@gmail.com>
2026-04-09 14:07:52 +0700

    hanh

--- src/main/java/pageobjects/HomePage.java
+++ src/main/java/pageobjects/HomePage.java
@@ -1,12 +1,25 @@
 package pageobjects;
 
 import common.Constant;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+import static common.Constant.WEBDRIVER;
 
 public class HomePage extends GeneralPage {
 
     // Mở trang chủ
     public HomePage open() {
-        Constant.WEBDRIVER.navigate().to(Constant.RAILWAY_URL);
+        WEBDRIVER.navigate().to(Constant.RAILWAY_URL);
         return this; // Trả về chính nó để có thể gọi hàm tiếp theo một cách gọn gàng
+    }
+
+
+    private final By _tabTimetable = By.linkText("Timetable");
+
+    // Method to navigate to the Timetable page
+    public TimeTable gotoTimeTable() {
+        WEBDRIVER.findElement(_tabTimetable).click();
+        return new TimeTable();
     }
 }
